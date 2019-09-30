50c4eab16acc28d5679949f250d8e88ec2b6f507
Intel <intel@Intel-Mac.local>
2019-09-30 12:05:27 +0530

    orderApi structure completed. data persisted on refresh

---
+++ precache-manifest.8f2b787ad35f2a4244b6715d39f585ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e63c90ae09cbe09cee9dd1d238a2b425",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb6b436bf4ee840ac09f",
+    "url": "/static/css/3.79e7f76d.chunk.css"
+  },
+  {
+    "revision": "49023e7c1d81c8deda05",
+    "url": "/static/css/main.97527a75.chunk.css"
+  },
+  {
+    "revision": "b5fe160a1759133547e5",
+    "url": "/static/js/2.ccc833b1.chunk.js"
+  },
+  {
+    "revision": "bb6b436bf4ee840ac09f",
+    "url": "/static/js/3.4881e87e.chunk.js"
+  },
+  {
+    "revision": "49023e7c1d81c8deda05",
+    "url": "/static/js/main.837aca69.chunk.js"
+  },
+  {
+    "revision": "1b60380bb118f2fb24e8",
+    "url": "/static/js/runtime-main.4df8a210.js"
+  },
+  {
+    "revision": "cf4e2601c3bc0b218216390d0e09d35a",
+    "url": "/static/media/quotes.cf4e2601.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9014ebda154c434c77765a5f1d3a8e73.js"
+  "/precache-manifest.8f2b787ad35f2a4244b6715d39f585ba.js"
 );
 
 self.addEventListener('message', (event) => {
